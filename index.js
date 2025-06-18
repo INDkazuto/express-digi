ad647189a2e03379895dd3cf0893e3815e6e1ae1
INDkazuto <kumarrohit1891821@gmail.com>
2025-06-18 19:04:54 +0530

    Add logging functionality with Winston and Morgan; update .gitignore and package files

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 let teaData = [];
 let nextId = 1;
 app.post("/tea", (req, res) => {
@@ -39,16 +60,15 @@ app.put("/tea/:id", (req, res) => {
   res.status(200).send(tea);
 
   console.log("updated");
-  
 });
 
 app.delete("/tea/:id", (req, res) => {
-  const index = teaData.find((t) => t.id === parseInt(req.params.id));
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
   if (index === -1) {
     return res.status(400).send("Tea not found");
   }
-  teaData.splice(index, 1)
-   return res.status(200).send("deleted")
+  teaData.splice(index, 1);
+  return res.status(200).send(`deleted ${teaData}`);
 });
 
 app.listen(port, () => {
